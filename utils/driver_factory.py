0978f401dbdf01e426c17a15007ecbcb03009c16
RamaChintharedd <ramadevi_chinthareddy@epam.com>
2026-03-14 16:45:41 +0530

    code pushed successfully.

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,38 +1,44 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from typing import Any
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service as FFService
+from selenium.webdriver.firefox.options import Options as FFOptions
 import shutil
 import os
 
-
-def create_driver(browser: str = "chrome", headless: bool = False) -> Any:
-    """Factory to create WebDriver instances. Keeps test code decoupled from driver details."""
-    browser = browser.lower()
-
-    if browser == "chrome":
-        options = ChromeOptions()
+# Minimal driver factory supporting Chrome and Firefox. Extendable for cloud providers.
+class DriverFactory:
+    @staticmethod
+    def create_chrome_driver(headless: bool = True):
+        chrome_opts = Options()
         if headless:
-            options.add_argument("--headless=new")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        # Allow user to supply CHROME_DRIVER_PATH env var; otherwise rely on webdriver-manager or PATH
-        chrome_path = os.getenv("CHROME_DRIVER_PATH")
-        if chrome_path and shutil.which(chrome_path):
-            service = ChromeService(executable_path=chrome_path)
-            return webdriver.Chrome(service=service, options=options)
-        return webdriver.Chrome(options=options)
+            chrome_opts.add_argument("--headless=new")
+        chrome_opts.add_argument("--window-size=1920,1080")
+        chrome_opts.add_argument("--no-sandbox")
+        chrome_opts.add_argument("--disable-dev-shm-usage")
 
-    if browser == "firefox":
-        options = FirefoxOptions()
+        # Attempt to locate chromedriver in PATH
+        chromedriver_path = shutil.which("chromedriver")
+        if chromedriver_path:
+            service = Service(chromedriver_path)
+            driver = webdriver.Chrome(service=service, options=chrome_opts)
+        else:
+            # If running in environment with bundled browser, webdriver-manager or other tooling should be used.
+            driver = webdriver.Chrome(options=chrome_opts)
+        return driver
+
+    @staticmethod
+    def create_firefox_driver(headless: bool = True):
+        ff_opts = FFOptions()
         if headless:
-            options.add_argument("-headless")
-        firefox_path = os.getenv("GECKO_DRIVER_PATH")
-        if firefox_path and shutil.which(firefox_path):
-            service = FirefoxService(executable_path=firefox_path)
-            return webdriver.Firefox(service=service, options=options)
-        return webdriver.Firefox(options=options)
+            ff_opts.headless = True
+        ff_opts.add_argument("--width=1920")
+        ff_opts.add_argument("--height=1080")
 
-    raise ValueError(f"Unsupported browser: {browser}")
+        geckodriver_path = shutil.which("geckodriver")
+        if geckodriver_path:
+            service = FFService(geckodriver_path)
+            driver = webdriver.Firefox(service=service, options=ff_opts)
+        else:
+            driver = webdriver.Firefox(options=ff_opts)
+        return driver

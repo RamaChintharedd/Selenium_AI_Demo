d94f607006019f55b10115460798bf4e6cfbbb12
RamaChintharedd <ramadevi_chinthareddy@epam.com>
2026-03-16 00:06:52 +0530

    Create src/utils/config.py

---
+++ src/utils/config.py
@@ -0,0 +1,7 @@
+# Centralized configuration values and selectors that may be reused across the tests.
+
+AMAZON_URL = 'https://www.amazon.com'
+DEMO_WEBSHOP_URL = 'https://demowebshop.tricentis.com'
+
+# Timeouts (seconds)
+DEFAULT_TIMEOUT = 15
